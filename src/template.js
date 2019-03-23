8b794660c062e32d0dbe28b5f194fdcb7e81e3bd
peakyDicers <ian.chui@hotmail.com>
2019-03-23 19:06:21 -0400

    added template

---
+++ src/template.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Template extends React.Component(){
+
+  myFunction = () => {
+    console.log('hello')
+  }
+
+  render(){
+    return(
+      <div>
+        <button onClick={}></button>
+        </div>
+
+    );
+  }
+}
+
+export default Template;
